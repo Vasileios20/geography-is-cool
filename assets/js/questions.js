88e9258573163d1d8b5e293228b95e70c4a4156f
vasileios20 <vasilis_tsim@outlook.com>
2023-05-24 20:24:13 +0100

    Add: add flag images folders and question/countries arrays for js

---
+++ assets/js/questions.js
@@ -0,0 +1,82 @@
+export let questions = [
+    {
+        question: "../assets/images/flags/gr.svg",
+        choice1: "Italy",
+        choice2: "Greece",
+        choice3: "France",
+        choice4: "Paraguay",
+        answer: 2
+    },
+    {
+        question: "../assets/images/flags/be.svg",
+        choice1: "Germany",
+        choice2: "Spain",
+        choice3: "Netherlands",
+        choice4: "Belgium",
+        answer: 4  
+    },
+    {
+        question: "../assets/images/flags/ca.svg",
+        choice1: "Canada",
+        choice2: "Peru",
+        choice3: "Mexico",
+        choice4: "Kenya",
+        answer: 1  
+    },
+    {
+        question: "../assets/images/flags/co.svg",
+        choice1: "Equador",
+        choice2: "Colombia",
+        choice3: "Chile",
+        choice4: "Venezuela",
+        answer: 2  
+    },
+    {
+        question: "../assets/images/flags/cz.svg",
+        choice1: "Austria",
+        choice2: "Slovakia",
+        choice3: "Chech Republic",
+        choice4: "Lithuania",
+        answer: 3  
+    },
+    {
+        question: "../assets/images/flags/pe.svg",
+        choice1: "Uruguay",
+        choice2: "Brazi",
+        choice3: "Argentina",
+        choice4: "Peru",
+        answer: 4  
+    },
+    {
+        question: "../assets/images/flags/pl.svg",
+        choice1: "Poland",
+        choice2: "Indonesia",
+        choice3: "Sweden",
+        choice4: "Lebanon",
+        answer: 1  
+    },
+    {
+        question: "../assets/images/flags/ly.svg",
+        choice1: "Liberia",
+        choice2: "Switzerland",
+        choice3: "Taiwan",
+        choice4: "Libya",
+        answer: 4  
+    },
+    {
+        question: "../assets/images/flags/sr.svg",
+        choice1: "Thailand",
+        choice2: "Togo",
+        choice3: "Suriname",
+        choice4: "Honduras",
+        answer: 3  
+    },
+    {
+        question: "../assets/images/flags/ng.svg",
+        choice1: "Nigeria",
+        choice2: "Nicaragua",
+        choice3: "Malaysia",
+        choice4: "Kenya",
+        answer: 1  
+    }
+];
