63bda9fb7ac6ee438596b018b4c6f03e0962cb2a
vasileios20 <vasilis_tsim@outlook.com>
2023-05-29 20:52:30 +0100

    Update code to return to index.html to end.js

--- assets/js/end.js
+++ assets/js/end.js
@@ -26,6 +26,6 @@ saveButton.addEventListener("click", (e) => {
 
     localStorage.setItem("highScores", JSON.stringify(highScores));
 
-    window.location.assign("/");
+    window.location.assign("index.html");
 
 });
